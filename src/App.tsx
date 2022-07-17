9c398d5402d7f84a006382583a1c6bea27befac2
Renato <renatomarquesteles@gmail.com>
2022-07-16 22:21:43 -0300

    adds global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>Hello world</div>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
