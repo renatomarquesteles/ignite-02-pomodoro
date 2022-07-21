17758f2186b532cb1a2b68dfb58df7dcaeb33185
Renato <renatomarquesteles@gmail.com>
2022-07-21 11:34:38 -0300

    adds form inputs styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export const Home = () => {
@@ -14,10 +16,28 @@ export const Home = () => {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I&apos;ll work on</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Name your project"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Task 1" />
+            <option value="Task 2" />
+            <option value="Task 3" />
+            <option value="Task 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">during</label>
-          <input id="minutesAmount" type="number" />
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutes.</span>
         </FormContainer>
